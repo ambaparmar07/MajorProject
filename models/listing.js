7aa6582a32832f914e3097a8cd951b4e38558099
ambaparmar07 <amba.parmar07@gmail.com>
2024-06-05 11:43:09 +0530

    second commit: CRUD Operations

--- models/listing.js
+++ models/listing.js
@@ -3,19 +3,21 @@ const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
     title: {
-        type:String,
+        type: String,
         required: true,
     },
     description: String,
-    
+
     image: {
         filename: {
             type: String,
-            required: true
+            default:
+                "https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
         },
         url: {
             type: String,
-            required: true
+
         }
     },
 
