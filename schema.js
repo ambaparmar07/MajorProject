dc65a335063f67311d8b37d13bdad0fb7e1aee69
ambaparmar07 <amba.parmar07@gmail.com>
2024-07-04 13:54:17 +0530

    error

--- schema.js
+++ schema.js
@@ -1,12 +1,13 @@
 const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
-    listing: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-        country: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().allow("", null),
-    }).required(),
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    price: Joi.number().required().min(0),
+    image: Joi.object({
+        filename: Joi.string(),
+        url: Joi.string().allow("", null).default("https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+    })
 });
